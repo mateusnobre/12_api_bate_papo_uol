e22fc63eb646350c542f5574f928aa4c447ff355
mateusnobre <mateusnobrests@gmail.com>
2021-06-13 03:33:29 -0300

    sanitizing messages and schema validation

--- src/server.js
+++ src/server.js
@@ -1,5 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import { stripHtml } from "string-strip-html";
+import Joi from 'joi';
+const messageSchema = Joi.object({
+    from: Joi.string().min(1),
+    to: Joi.string().min(1),
+    text: Joi.string().min(1),
+    type: Joi.string().min(1),
+    time: Joi.string().min(1)
+})
+
+const participantSchema = Joi.object({
+    name: Joi.string().min(1),
+    lastStatus: Joi.number().integer()
+})
 
 const app = express();
 app.use(cors());
@@ -9,13 +23,8 @@ const participants = [];
 const messages = [];
 
 app.post("/participants", (req, res) => {
-    if (typeof req.body == undefined){
-        res.status(404).send("You can't send a empty body");
-    }
-    else if (typeof req.body.name == ''){
-        res.status(404).send("You can't send a empty string as name");
-    }
-    else {
+    try {
+        participantSchema.validate(req.body)
         let participant = {
             name: '',
             lastStatus: '',
@@ -33,16 +42,40 @@ app.post("/participants", (req, res) => {
         message.text = 'entra na sala...'
         message.type = 'status'
         message.time = (new Date()).toLocaleTimeString()
-        participant.name = newMessage.name;
+        participant.name = trim(stripHtml(newMessage.name));
         participant.lastStatus = Date.now();
         participants.push(participant);
         messages.push(message)
         res.status(200).send('Usuário Cadastrado');
     }
+    catch (error){
+        res.status(404).send("You can't send a empty string as name");
+    }
 })
 
 app.get("/participants", (req, res) => {
     res.send(participants);
+})
+
+app.post("/messages", (req, res) => {
+    try{
+    let message = {
+        from: '',
+        to: '',
+        text: '',
+        type: '',
+        time: ''
+    }
+    const newMessage = req.body
+    message.from = trim(stripHtml(req.headers.User))
+    message.to = trim(stripHtml(newMessage.to))
+    message.text = trim(stripHtml(newMessage.text))
+    message.type = trim(stripHtml(newMessage.type))
+    message.time = (new Date()).toLocaleTimeString()
+    res.status(200).send('Messagem enviada')}
+    catch (error){
+        res.status(404).send('Erro na validação dos dados')
+    }
 })
 
 app.listen(process.env.PORT || 4000) 
