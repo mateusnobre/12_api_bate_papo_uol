3b031abbfd6f0a9daae7c20c731672672edc21b4
mateusnobre <mateusnobrests@gmail.com>
2021-06-13 01:23:45 -0300

    post participants

--- src/server.js
+++ src/server.js
@@ -4,4 +4,34 @@ import cors from 'cors';
 const app = express();
 app.use(cors())
 
+const participants = [];
+const messages = [];
+
+let participant = {
+    name: '',
+    lastStatus: '',
+}
+let message = {
+    from: '',
+    to: '',
+    text: '',
+    type: '',
+    time: ''
+}
+app.post("/participants", (req, res) => {
+    if (req.body.name = ''){
+        res.status(404).send("You can't send a empty string as name");
+    }
+    else {
+        participant.name = req.body;
+        participant.lastStatus = Date.now();
+        participants.push(participant);
+        res.send(participant);
+    }
+})
+
+app.get("/participants", (req, res) => {
+    res.send(participants);
+})
+
 app.listen(process.env.PORT || 4000)
